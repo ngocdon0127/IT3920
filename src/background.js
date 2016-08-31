f1a053f10c56db998101334496b334ba6fc8602a
Ngoc Don <kaitoukid2701@gmail.com>
2016-08-31 21:16:32 +0700

    Delete redudant functions

--- src/background.js
+++ src/background.js
@@ -7,12 +7,6 @@ var sourceTabId = '';
 // use this key to encrypt and decrypt attachment.
 var aesKeyFile = '';
 
-// chrome.browserAction.onClicked.addListener(function (tab) {
-// 	chrome.tabs.create({url: '/src/key-list.html'}, function (tab) {
-// 		// body...
-// 	});
-// })
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	if (request.actionType == 'open-encrypt-frame'){
 		// Save Gmail tab id.
@@ -25,10 +19,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 			type: 'panel'
 		});
 	}
-	// else if (request.actionType == 'decrypt-email'){
-	// 	console.log('catch decrypt-email in above');
-	// 	clickHandler(request.cipher);
-	// }
 
 	// email editor window establishes a port to request email content from Gmail tab.
 	chrome.extension.onConnect.addListener(function (port) {
@@ -57,46 +47,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 		})
 	});
 });
-
-// add context menu
-// chrome.contextMenus.create({
-// 	title: "Decrypt this message.",
-// 	contexts: ["selection"],
-// 	onclick: clickHandler
-// });
-
-// Context Menu click handler
-// function clickHandler (data, tab) {
-// 	chrome.windows.create({
-// 		url: "/src/decrypt-email.html",
-// 		// type: "panel"
-// 	});
-// 	if ((typeof(data) == 'object') && ('selectionText' in data) && data.selectionText.length > 0){
-// 		data = data.selectionText;
-// 	}
-// 	chrome.extension.onConnect.addListener(function(port) {
-// 		port.postMessage({
-
-// 			/**
-// 			 * Character ZERO WIDTH SPACE (unicode u200B - 8203) 
-// 			 * sometimes appears in selectionText when user double click.
-// 			 * remove it and trim() string before sending to decrypt-email.html
-// 			 */
-// 			data: data.replace(/\u200B/g, '').trim()
-// 		});
-// 	});
-// }
-
-// // Get Gmail address
-// function getCurrentGmailAddr () {
-// 	alert('start hehe');
-// 	var e = document.getElementsByName('tôi');
-// 	alert(e.length);
-// 	if (e.length < 1){
-// 		e = document.getElementsByName('me');
-// 	}
-// 	alert(e[0].getAttribute('email'));
-// }
 
 chrome.extension.onConnect.addListener(function (port){
 	port.onMessage.addListener(function (msg) {
@@ -151,30 +101,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 			}
 		})
 	}
-	else if (request.actionType === 'check-recipients-exist'){
-		var data = request;
-		console.log(data);
-		$.ajax({
-			url: SERVER + SERVER_PORT + '/E2EE/user/checkExistedUsers',
-			type: 'POST',
-			data: JSON.stringify(data),
-			beforeSend: function(xhr) {
-				xhr.setRequestHeader("Accept", "application/json");
-				xhr.setRequestHeader("Content-Type", "application/json");
-			},
-			success: function(data) { 
-				// alert("success");
-				sendResponse({
-					name: 'recipients-status',
-					data: data
-				});
-				return false;
-			},
-			error:function(data,status,er) { 
-				alert("error");
-			}
-		})
-	}
 	else if (request.actionType === 'request-public-key'){
 		console.log('receive request-public-key request');
 		var data = request;
@@ -204,49 +130,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 			}
 		});
 	}
-	else if (request.actionType === 'register-multiple-users'){
-		// handle later
-		var data = request;
-		console.log(data);
-		var ru = data['requestedUsers'];
-		for (var i = 0; i < ru.length; i++) {
-
-			// 123123 is implicit
-			ru[i].password = CryptoJS.MD5('123123').toString(CryptoJS.enc.Base16);
-			var rsa = generateRSAKey(ru[i].email, CryptoJS.MD5(ru[i].email).toString(CryptoJS.enc.Base16), ru[i].password, 1024);
-			ru[i].publicKey = rsa.public;
-			ru[i].encryptedPrivateKey = rsa.private;
-		}
-		jQuery.ajax({
-			url: SERVER + SERVER_PORT + '/E2EE/user/registerUsers',
-			type: 'POST',
-			data: JSON.stringify(data),
-			beforeSend: function(xhr) {
-				xhr.setRequestHeader("Accept", "application/json");
-				xhr.setRequestHeader("Content-Type", "application/json");
-			},
-			success: function(data) { 
-				// alert("success");
-				console.log(data);
-				if (data.result === 'success'){
-					for (var i = 0; i < ru.length; i++) {
-
-						// 123123 is implicit
-						delete ru[i].password;
-						delete ru[i].encryptedPrivateKey;
-					}
-					sendResponse({
-						name: 'send-registered-users',
-						data: ru
-					});
-				}
-				return false;
-			},
-			error:function(data,status,er) { 
-				alert("error");
-			}
-		});
-	}
 	else if (request.actionType === 'open-add-attachments-frame'){
 		// Save Mail tab id.
 		chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
@@ -261,11 +144,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 		sendResponse({
 			aesKeyFile: aesKeyFile
 		});
-	}
-	else if (request.actionType === 'decrypt-email'){
-		console.log('catch decrypt-email in under');
-		console.log(request.cipher);
-		clickHandler(request.cipher);
 	}
 	else if (request.actionType === 'verify'){
 		console.log('verify request');
